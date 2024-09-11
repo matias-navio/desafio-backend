da619af6495862262fb79de483477e13c19603c6
Matias Navio <naviomatias2@gmail.com>
2024-09-11 19:58:13 -0300

    Initial commit

---
+++ src/test/java/com/matias/desafio_backend/desafio_backend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matias.desafio_backend.desafio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
